00efeeb453653035531a5b1997e7d3d433a5dfe8
nlukic97 <nlukic97@gmail.com>
2020-06-25 11:47:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var ws = require('ws');
+var port = 3000;
+
+app.listen(port, ()=>{
+  console.log('Served on port: ' + port);
+})
+app.use(express.static('./public'));
+
+var server = new ws.Server({port: 3200});
+
+server.on('connection', (client)=>{
+  console.log('A user has connected.');
+
+  client.on('close',()=>{
+    console.log('A user has disconnected.');
+  })
+
+  client.on('message',(msg)=>{
+    // console.log('Client says ' + msg);
+    // console.log('Sending client: ' + msg)
+    // client.send(msg);
+    server.clients.forEach((client)=>{
+      client.send(msg)
+    })
+  })
+})
