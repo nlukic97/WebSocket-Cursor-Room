c31221aceea3366096a0ba27ea0b2c7f1a933c9d
nlukic97 <nlukic97@gmail.com>
2020-06-25 12:02:52 +0200

    ideas

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ var server = new ws.Server({port: 3200});
 
 server.on('connection', (client)=>{
   console.log('A user has connected.');
-
+  console.log(server.clients.size)
+  
   client.on('close',()=>{
     console.log('A user has disconnected.');
+    console.log(server.clients.size)
   })
 
-  client.on('message',(msg)=>{
+  client.on('message',(msg)=>{  
     // console.log('Client says ' + msg);
     // console.log('Sending client: ' + msg)
     // client.send(msg);
