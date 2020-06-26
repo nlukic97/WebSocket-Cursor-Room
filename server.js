62fa01c9c56693f46d2a12a08cab250b029bd61b
nlukic97 <nlukic97@gmail.com>
2020-06-26 15:10:41 +0200

    finished, just trying to figure out how to remove div after client leaves

--- server.js
+++ server.js
@@ -12,23 +12,39 @@ var server = new ws.Server({port: 3200});
 var CLIENTS = [];
 server.on('connection', (client, req)=>{
   CLIENTS.push(client)  
-  console.log('User ' + CLIENTS.indexOf(client) + ' has logged in.') //after 11 we 
+  console.log('User ' + CLIENTS.indexOf(client) + ' has logged in.')
   
+  var r = Math.floor(Math.random() * Math.floor(255));
+  var g = Math.floor(Math.random() * Math.floor(255));
+  var b = Math.floor(Math.random() * Math.floor(255));
+  client.backgroundColor = "rgb(" + r + "," + g + "," + b+ ")";
+  console.log(client.backgroundColor);
+
   CLIENTS.forEach(client => {
     client.on('close',(index)=>{
-      // console.log('A user has disconnected.');
       console.log('User ' + CLIENTS.indexOf(client) + ' has disconnected.');
-      }).setMaxListeners(0)  
+
+      ///////////////////////////////////////////////////////   <--- kako da posaljem id, pa da taj div uklonim?
+      
+      // var removeId = '{"removeId":' + CLIENTS.indexOf(client) + '}'
+      // console.log("Sending all clients: '"+removeId+"'")
+
+      //   server.clients.forEach(remainingClient =>{
+      //     remainingClient.send(this.removeId)
+      //   })
+
+        ///////////////////////////////////////////////////////
+      }).setMaxListeners(0)
+
     }
   )
 
   client.on('message',(msg)=>{
     var newMsg = msg.slice(0,-1,0)
-    newMsg = newMsg +  ', "id":' + CLIENTS.indexOf(client) +'}';
+    newMsg = newMsg +  ', "id":' + CLIENTS.indexOf(client) + ',"backgroundColor":'+ '"' +  client.backgroundColor + '"' +'}';
 
     console.log('Client ' + CLIENTS.indexOf(client) + ' says: ' + msg)
     console.log('Sending Clients: ' + newMsg)
-    // client.send(newMsg); //ovo ce samo poslati jednom
     server.clients.forEach(client =>{ //ovo salje svima
       client.send(newMsg)
     })
